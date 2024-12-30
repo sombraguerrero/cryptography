3e317c599ae965a8d46ee685103f5c8ea5d4b95d
Robert Setter <r.setter@comcast.net>
2024-12-29 23:29:12 -0600

    Fixing logic with existing key files

--- aes_gcm.py
+++ aes_gcm.py
@@ -90,8 +90,7 @@ class AES_Util(Tk):
                 with open(AES_Util.DefaultKeyPath, 'wb') as file: dump(crypto2, file)
             else:
                 with open(AES_Util.DefaultKeyPath, 'rb') as file: crypto = load(file)
-                crypto2 = {'authData': uuid4().bytes, 'nonce': urandom(12), 'key': crypto['key']}
-                with open(AES_Util.DefaultKeyPath, 'wb') as file2: dump(crypto2, file2)
+                crypto2 = {'authData': crypto['authData'], 'nonce': crypto['nonce'], 'key': crypto['key']}
         else:
             keyLoc = self.open_save_key(False)
             with open(keyLoc, 'rb') as file: crypto = load(file)
